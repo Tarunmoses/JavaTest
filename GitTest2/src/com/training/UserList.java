a114996fd1149e15a0e4ca21afaf5fe32ab21161
tarunmoses <tarunmoses99@gmail.com>
2021-10-19 09:29:02 +0530

    getter and setter in userlist

--- GitTest2/src/com/training/UserList.java
+++ GitTest2/src/com/training/UserList.java
@@ -4,5 +4,13 @@ import java.util.List;
 
 public class UserList {
 	List<User> userlist;
+
+	public List<User> getUserlist() {
+		return userlist;
+	}
+
+	public void setUserlist(List<User> userlist) {
+		this.userlist = userlist;
+	}
 	
 }
